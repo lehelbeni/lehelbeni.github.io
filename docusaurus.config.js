dc06a2c36416c8a55fd17e21b730125e4a7d958b
Beni Lehel <benilehel@Benis-MacBook-Pro.local>
2022-11-18 19:12:57 +0200

    first

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
   url: "https://lehelbeni.github.io",
-  baseUrl: "/lehelbeni.github.io/",
+  baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
