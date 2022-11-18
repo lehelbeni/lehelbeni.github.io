d0307a3e21cb85e949f6d49551036b3fd6686f37
Beni Lehel <benilehel@Benis-MacBook-Pro.local>
2022-11-18 19:08:19 +0200

    first

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "https://github.com",
+  url: "https://lehelbeni.github.io",
   baseUrl: "/lehelbeni.github.io/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
